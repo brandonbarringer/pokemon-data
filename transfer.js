cf4007c662bd0ac01c5c89cc36d74fac0c8c1430
Brandon Barringer <barringerbrandon@gmail.com>
2019-06-13 16:24:00 -0700

    file cleanup

--- transfer.js
+++ transfer.js
@@ -1,8 +1,5 @@
 const oldDB = require('./pokemon-team-builder-8cf49-export.json');
 const _ = require('underscore');
-// const config = require('./config.js')
-// const firebase = require('firebase');
-
 const admin = require('firebase-admin')
 const functions = require('firebase-functions')
 let serviceAccount = require('./firebaseServiceKey.json')
@@ -13,23 +10,8 @@ admin.initializeApp({
 
 let db = admin.firestore();
 
-// const firebaseConfig = config.config;
-// const db = firebase.firestore();
-// Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-
-
-// const db = JSON.parse(oldDB)
-
-// console.log(oldDB.pokemon[Object.keys(oldDB.pokemon)[0]]);
-
-// Object.keys(oldDB.pokemon).map((key, index) => {
-// 	if (index > 1) return
-// 	let value = Object[key]
-// 	console.log(value)
-// })
-
 Object.keys(oldDB.pokemon).forEach((key, index) => {
+	// only the ones we want
 	if (index <= 600 || index > 807) {
 		return
 	}
@@ -41,5 +23,4 @@ Object.keys(oldDB.pokemon).forEach((key, index) => {
 	delete oldDB.pokemon[key];
 	// write the document to the database
 	db.collection('pokemon').doc(newKey).set(oldDB.pokemon[newKey])
-	// console.log(index)
 })
